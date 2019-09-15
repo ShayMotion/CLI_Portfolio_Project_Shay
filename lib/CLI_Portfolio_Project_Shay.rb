397573777fbf25f29246df4e9dbe245220c05d61
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-09-15 18:13:14 +0000

    first commit

---
+++ lib/CLI_Portfolio_Project_Shay.rb
@@ -0,0 +1,6 @@
+require "CLI_Portfolio_Project_Shay/version"
+
+module CLIPortfolioProjectShay
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/CLI_Portfolio_Project_Shay/version.rb
@@ -0,0 +1,3 @@
+module CLIPortfolioProjectShay
+  VERSION = "0.1.0"
+end
